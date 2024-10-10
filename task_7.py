6528eb1b9138847435b4c3bb3a2d3bfeca23f9f4
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2024-10-10 20:09:24 +0300

    решил задачу 7

--- task_7.py
+++ task_7.py
@@ -1,1 +1,22 @@
+# Напишите программу, которая получает на вход количество уровней пирамиды и выводит их на экран, заполняя нечётными числами:
 
+height = int(input("Введите высоту пирамиды: "))
+number = 1
+
+for y in range(height):
+    for x in range(-height, height * 2):
+
+        if x >= height // 2 - y and x <= height // 2 + y:
+            print(number, end='\t')
+            number += 2
+            
+        elif x == height // 2:
+            print(number, end='\t')
+            number += 2
+
+        else:
+            print(' ', end='\t')
+        
+    print()
+
+# По готовой 6 задаче, добавил переменную для вывода в место '#'
